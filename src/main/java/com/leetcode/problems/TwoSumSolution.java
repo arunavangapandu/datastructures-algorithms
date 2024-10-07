0a41465e48b2689c77646738fd10a73e4ce28292
arunavangapandu <arunavangapandu2016@gmail.com>
2024-10-07 11:51:31 -0700

    Added group of Anagrams code

--- src/main/java/com/leetcode/problems/TwoSumSolution.java
+++ src/main/java/com/leetcode/problems/TwoSumSolution.java
@@ -5,23 +5,21 @@ import java.util.HashMap;
 
 public class TwoSumSolution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> prevMap = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            int diff = target - num;
-
-            if (prevMap.containsKey(diff)) {
-                return new int[] { prevMap.get(diff), i };
+            int compliment = target - num;
+            if(map.containsKey(compliment)) {
+                return new int[] {map.get(compliment), i};
             }
-
-            prevMap.put(num, i);
+            map.put(num, i);
         }
+        return new int[]{};
 
-        return new int[] {};
     }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new TwoSumSolution().twoSum(new int[] {4,5,6}, 10)));
+        System.out.println(Arrays.toString(new TwoSumSolution().twoSum(new int[] {3,4,5,6}, 7)));
     }
 }

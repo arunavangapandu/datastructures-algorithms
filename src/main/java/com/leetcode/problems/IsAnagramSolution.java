e2faada065f536521b1745be20fd309d49dcc164
arunavangapandu <arunavangapandu2016@gmail.com>
2024-10-07 11:07:51 -0700

    Modified isAnagram solution code

--- src/main/java/com/leetcode/problems/IsAnagramSolution.java
+++ src/main/java/com/leetcode/problems/IsAnagramSolution.java
@@ -6,31 +6,29 @@ import java.util.HashMap;
 public class IsAnagramSolution {
 
     public boolean isAnagram(String s, String t) {
-
-        if(s.length() != t.length()) {
+        if(s.length() != t.length()){
             return false;
         }
 
-        HashMap<Character, Integer> count = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c  = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            }
 
-
-            // Increment count for character in s
-            count.put(charS, count.getOrDefault(charS, 0) + 1);
-
-            // Decrement count for character in t
-            count.put(charT, count.getOrDefault(charT, 0) - 1);
-        }
-
-        // Check if all counts are 0
-        for (int value : count.values()) {
-            if (value != 0) {
-                return false;
+        for(int j = 0; j < t.length(); j++) {
+            char c = t.charAt(j);
+            if(map.containsKey(c)) {
+                map.put(c, map.getOrDefault(c, 0) - 1);
             }
         }
 
+
+        for(int value:map.values()) {
+            if(value != 0) {
+                return false;
+            }
+        }
         return true;
     }
 

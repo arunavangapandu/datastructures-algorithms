bf31140714b33f9427811d96f25e52f2ecaca822
arunavangapandu <arunavangapandu2016@gmail.com>
2024-11-16 16:12:18 -0800

    Modified code

--- src/main/java/com/leetcode/problems/medium/MinStack.java
+++ src/main/java/com/leetcode/problems/medium/MinStack.java
@@ -33,4 +33,15 @@ public class MinStack {
     public int getMin() {
         return minStack.peek();
     }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+    }
 }
